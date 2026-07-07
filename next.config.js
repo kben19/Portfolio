524dfe24866aaadf54c1103519fb26c29cadeb46
Kelvin Benzali <kevinesia@gmail.com>
2026-07-07 15:31:07 +0700

    Update next.config.js for unoptimized image

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"], // prefer modern formats
     remotePatterns: [
       // Circular country flags on the About dashboard's Countries tab
